25c655a4eb101236db77d55fdeb62b780c52b6a8
Kody Brown <thewizard@wasatchwizard.com>
2015-10-31 15:39:10 -0600

    Updated default namespace in project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,14 +8,14 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyTitle("seth ver:1.0.11.51031")]
+[assembly: System.Reflection.AssemblyTitle("seth ver:1.0.13.51031")]
 [assembly: System.Reflection.AssemblyDescription("seth")]
 [assembly: System.Reflection.AssemblyProduct("seth")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright (C) 2003-2015 Kody Brown (@kodybrown).")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(false)]
 [assembly: System.Runtime.InteropServices.Guid("0F6C111B-9FE1-4828-BFCE-729E2CB8A012")]
-[assembly: System.Reflection.AssemblyVersion("1.0.11.51031")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.11.51031")]
+[assembly: System.Reflection.AssemblyVersion("1.0.13.51031")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.13.51031")]
 
 
